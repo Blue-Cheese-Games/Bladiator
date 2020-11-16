f95b86be099f195ed939ca7b23f302145aad6703
Aaron Knoop <aaron.knoop@live.nl>
2020-11-16 15:28:55 +0100

    Udated Player.cs

--- Assets/_Scripts/Entity/Player/Player.cs
+++ Assets/_Scripts/Entity/Player/Player.cs
@@ -1,23 +1,22 @@
 ﻿using Bladiator.Entities;
-using Bladiator.Entity.Player;
 using Bladiator.Managers;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Player : EntityBase
+namespace Bladiator.Entity.Player
 {
-    private PlayerController m_PlayerController;
+	public class Player : EntityBase
+	{
+		private PlayerController m_PlayerController;
 
-    protected override void Awake()
-    {
-        base.Awake();
+		protected override void Awake()
+		{
+			base.Awake();
 
-        GameManager.Instance.AddPlayer(this);
-    }
+			GameManager.Instance.AddPlayer(this);
+		}
 
-    public override void Move()
-    {
-        // Movement.
-    }
+		public override void Move()
+		{
+			// Movement.
+		}
+	}
 }
