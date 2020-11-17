597f9b5f6ffca57f8fe571dcc4acf0db6a42a517
Aaron Knoop <aaron.knoop@live.nl>
2020-11-17 12:29:46 +0100

    Simple look at camera and added simple player movement

--- Assets/_Scripts/Entity/Player/PlayerController.cs
+++ Assets/_Scripts/Entity/Player/PlayerController.cs
@@ -1,14 +1,34 @@
-using System;
 using UnityEngine;
 
 namespace Bladiator.Entity.Player
 {
-	[RequireComponent(typeof(Player))]
 	public class PlayerController : MonoBehaviour
 	{
+		[SerializeField] float m_MovementSpeed = 1f;
+		[SerializeField] GameObject m_SpriteObject;
 		public void MoveHandle()
 		{
+			if (Camera.main == null || m_SpriteObject == null) return;
+			
+			m_SpriteObject.transform.forward = -Camera.main.transform.forward;
+			
+			InputHandle();
+		}
 
+		private void InputHandle()
+		{
+			float horizontalAxis = Input.GetAxisRaw("Horizontal");
+			float verticalAxis = Input.GetAxisRaw("Vertical");
+			
+			Vector3 axis = new Vector3(horizontalAxis, 0, verticalAxis);
+			
+			if (axis != Vector3.zero)
+			{
+				Vector3 targetPos = axis * (m_MovementSpeed * Time.deltaTime);
+				targetPos.y = transform.position.y;
+
+				transform.position += targetPos;
+			}
 		}
 	}
 }
