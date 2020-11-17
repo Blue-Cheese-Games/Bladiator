8124281e9147bebcbfc1fd14215bf4aa9cb66645
Lars <larsbeijaard@outlook.com>
2020-11-17 15:11:13 +0100

    Weapon object with all data (damage etc)

---
+++ Assets/_Scripts/Weapon/WeaponObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Bladiator.Weapons
+{
+    [CreateAssetMenu(fileName = "Weapon", menuName = "Weapon/WeaponBase", order = 0)]
+    public class WeaponObject : ScriptableObject
+    {
+        public WeaponCoreData WeaponCoreData;
+        public WeaponAestheticData WeaponAestheticData;
+    }
+    
+    [System.Serializable]
+    public struct WeaponCoreData
+    {
+        public float Damage;
+        public float acceleration;
+    }
+    
+    [System.Serializable]
+    public struct WeaponAestheticData
+    {
+        public Sprite Sprite;
+        public ParticleSystem HitParticle;
+        public AudioClip HitAudio;
+    }
+}
