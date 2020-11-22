8c860207a536c7f6c61d3b6418e7d67c9ab25428
Aaron Knoop <aaron.knoop@live.nl>
2020-11-22 15:48:59 +0100

    Updated player to use rigidbody physics

--- Assets/_Scripts/Entity/Player/PlayerController.cs
+++ Assets/_Scripts/Entity/Player/PlayerController.cs
@@ -1,33 +1,44 @@
+using System;
 using UnityEngine;
 
 namespace Bladiator.Entity.Player
 {
+	[RequireComponent(typeof(Rigidbody))]
 	public class PlayerController : MonoBehaviour
 	{
-		[SerializeField] float m_MovementSpeed = 1f;
-		[SerializeField] GameObject m_SpriteObject;
-		
+		[SerializeField] private float m_MovementSpeed = 1f;
+		[SerializeField] private GameObject m_SpriteObject;
+
+		private Rigidbody m_Rig;
+
+		private void Start()
+		{
+			m_Rig = GetComponent<Rigidbody>();
+		}
+
 		public void MoveHandle()
 		{
 			if (Camera.main == null || m_SpriteObject == null) return;
 			
-			m_SpriteObject.transform.forward = -Camera.main.transform.forward;
+			// m_SpriteObject.transform.forward = -Camera.main.transform.forward;
 			
 			InputHandle();
 		}
 
 		private void InputHandle()
 		{
-			float horizontalAxis = Input.GetAxisRaw("Horizontal");
-			float verticalAxis = Input.GetAxisRaw("Vertical");
+			float horizontalAxis = -Input.GetAxisRaw("Horizontal");
+			float verticalAxis = -Input.GetAxisRaw("Vertical");
 			
 			Vector3 axis = new Vector3(horizontalAxis, 0, verticalAxis);
 			
 			if (axis != Vector3.zero)
 			{
-				Vector3 targetPos = axis * (m_MovementSpeed * Time.deltaTime);
-
-				transform.position += targetPos;
+				m_Rig.velocity = axis * m_MovementSpeed;
+			}
+			else
+			{
+				m_Rig.velocity = Vector3.zero;
 			}
 		}
 	}
