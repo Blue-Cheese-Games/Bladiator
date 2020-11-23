9b8537184768b3ec737bc55fb939b6b4bdc13fc6
Aaron Knoop <aaron.knoop@live.nl>
2020-11-23 13:05:55 +0100

    Added camera controller to test the camera rotation

---
+++ Assets/_Scripts/Camera/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float m_RotateSpeed;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles += Vector3.up * (Time.deltaTime * m_RotateSpeed);
+    }
+}
