5120f6d622d76eb3332ee8083618339979c1319e
Aaron Knoop <aaron.knoop@live.nl>
2020-11-23 15:01:18 +0100

    Added camera movement

--- Assets/_Scripts/Camera/CameraController.cs
+++ Assets/_Scripts/Camera/CameraController.cs
@@ -1,12 +1,42 @@
 ﻿using UnityEngine;
 
+public enum CameraState
+{
+    Idle,
+    Moving
+}
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float m_RotateSpeed;
+    private CameraState m_CameraState;
     
+    [SerializeField] private float m_MouseXCurrent = 0;
+    [SerializeField] private float m_MouseXPrevious = 0;
+
+    public CameraState CameraState => m_CameraState;
+
     // Update is called once per frame
     void Update()
     {
-        transform.eulerAngles += Vector3.up * (Time.deltaTime * m_RotateSpeed);
+        if (Input.GetMouseButtonDown(2))
+        {
+            m_MouseXPrevious = Input.mousePosition.x;
+            m_MouseXCurrent = Input.mousePosition.x;
+            m_CameraState = CameraState.Moving;
+        }
+        
+        if (Input.GetMouseButton(2))
+        {
+            m_MouseXCurrent = Input.mousePosition.x;
+
+            float delta = m_MouseXCurrent - m_MouseXPrevious;
+            transform.eulerAngles += new Vector3(0, delta, 0);
+
+            m_MouseXPrevious = m_MouseXCurrent;
+        }
+
+        if (Input.GetMouseButtonUp(2))
+        {
+            m_CameraState = CameraState.Idle;
+        }
     }
 }
