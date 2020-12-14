61fb32f7bb26c95f2c13e10a2e4331e692fa8ec5
Aaron Knoop <aaron.knoop@live.nl>
2020-12-14 15:10:40 +0100

    Fixed spam

--- Assets/_Scripts/Weapon/WeaponBase.cs
+++ Assets/_Scripts/Weapon/WeaponBase.cs
@@ -14,12 +14,23 @@ namespace Bladiator.Weapons
 		private Vector3 m_StartPosition;
 		private Quaternion m_StartRotation;
 
+		private Vector3 m_TargetPosition;
+
+		private float m_WarmupCooldown = .5f;
+		private float m_WarmupTimer = 0f;
+
+		private bool m_SecondAttack;
+		private bool m_WaitSecondAttack;
+
+		private float m_AttackCooldown = .25f;
+		private float m_AttackCooldownTimer = 0f;
+		
 		private void Awake()
 		{
 			m_Weapon = GetComponent<Weapon>();
 		}
 
-		void Start()
+		private void Start()
 		{
 			GetComponentInChildren<Hitbox>().OnHit += OnHit;
 
@@ -45,32 +56,56 @@ namespace Bladiator.Weapons
 			if (MouseManager.Instance.HasHit())
 				Move();
 
-			if (Input.GetMouseButtonDown(0) && !m_Attack)
+			Rotate();
+
+			if (m_AttackCooldownTimer > 0)
+			{
+				m_AttackCooldownTimer -= Time.deltaTime;
+				return;
+			}
+			
+			if (Input.GetMouseButtonDown(0) && !m_Attack && !m_SecondAttack && !m_WaitSecondAttack)
 			{
 				m_Attack = true;
 			}
 
-			Rotate();
+			if (!m_SecondAttack && m_WaitSecondAttack && m_WarmupTimer < m_WarmupCooldown)
+			{
+				if (Input.GetMouseButtonDown(0))
+				{
+					m_WaitSecondAttack = false;
+					m_SecondAttack = true;
+				}
+
+				m_WarmupTimer += Time.deltaTime;
+			}
+			else
+			{
+				m_WaitSecondAttack = false;
+				m_WarmupTimer = 0;
+			}
 		}
 
 		private void OnHit(Collider other)
 		{
-			if (other.CompareTag("Player") || !m_Attack) return;
+			if (other.CompareTag("Player")) return;
 
-			try
+			if (m_Attack || m_SecondAttack)
 			{
-				EntityBase entity = other.GetComponent<EntityBase>();
-				entity.Damage((int) m_Weapon.WeaponObject.WeaponData.Damage);
-				m_Weapon.HitParticle.Play();
-			}
-			catch (Exception exception)
-			{
-				Debug.Log("Object is not an entity");
+				try
+				{
+					EntityBase entity = other.GetComponent<EntityBase>();
+					entity.Damage((int) m_Weapon.WeaponObject.WeaponData.Damage);
+					m_Weapon.HitParticle.Play();
+				}
+				catch (Exception exception)
+				{
+					Debug.Log("Object is not an entity");
+				}
 			}
 		}
 
-		private Vector3 m_TargetPosition;
-		
+		//TODO This needs some improvement for the secondary attack.
 		private void Move()
 		{
 			Vector3 playerPos = m_Weapon.Player.transform.position;
@@ -78,32 +113,44 @@ namespace Bladiator.Weapons
 
 			// Clamp the weapon position inside a circle
 			Vector3 offset = newPos - playerPos;
-			Vector3 position = playerPos + Vector3.ClampMagnitude(offset, m_Weapon.WeaponObject.WeaponData.Reach);
+			Vector3 position = playerPos + offset;
 
 			position.y = 1.5f;
 
-			if (m_Attack)
+			if (m_Attack || m_SecondAttack)
 			{
 				if (m_TargetPosition == Vector3.zero)
 				{
 					m_TargetPosition = position;
-				} 
-				
+				}
+
 				if (Vector3.Distance(transform.position, m_TargetPosition) > 0.5f)
 				{
 					transform.position = Vector3.Lerp(transform.position,
 						m_TargetPosition, m_Weapon.WeaponObject.WeaponData.AttackDragVelocity * Time.deltaTime);
 				}
+				else if (!m_SecondAttack)
+				{
+					m_WaitSecondAttack = true;
+					m_Attack = false;
+					m_TargetPosition = Vector3.zero;
+				}
 				else
 				{
+					m_WaitSecondAttack = false;
+					m_SecondAttack = false;
 					m_TargetPosition = Vector3.zero;
-					m_Attack = false;
+					m_AttackCooldownTimer = m_AttackCooldown;
 				}
 			}
 			else
 			{
-				transform.position = Vector3.Lerp(transform.position,
-					position, m_Weapon.WeaponObject.WeaponData.IdleDragVelocity * Time.deltaTime);
+				if (!m_WaitSecondAttack)
+				{
+					transform.position = Vector3.Lerp(transform.position,
+						(playerPos + new Vector3(.75f, 0, .75f)),
+						m_Weapon.WeaponObject.WeaponData.AttackDragVelocity * Time.deltaTime);
+				}
 			}
 		}
 
