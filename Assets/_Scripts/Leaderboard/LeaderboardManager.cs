7c1aae3c35e5c6be1aa656a1e9ef42d07ca24c27
Lars <larsbeijaard@outlook.com>
2021-01-05 16:51:32 +0100

    Added sorting algorithm to sort the leaderboard

--- Assets/_Scripts/Leaderboard/LeaderboardManager.cs
+++ Assets/_Scripts/Leaderboard/LeaderboardManager.cs
@@ -1,11 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Bladiator.Leaderboard
 {
     public class LeaderboardManager : MonoBehaviour
     {
-        [SerializeField] private List<string> m_LeaderboardItem = new List<string>();
+        [SerializeField] private List<LeaderboardItem> m_LeaderboardItem = new List<LeaderboardItem>();
+
+        private void Awake()
+        {
+            SortLeaderboard();
+        }
+
+        private void SortLeaderboard()
+        {
+            // Loop though each of the leaderboard items and sort them by score count
+            for (int i = 0; i < m_LeaderboardItem.Count; i++)
+            {
+                for (int j = 0; j < m_LeaderboardItem.Count - 1; j++)
+                {
+                    // Swap to indexes
+                    if (m_LeaderboardItem[j].Score < m_LeaderboardItem[j + 1].Score)
+                    {
+                        LeaderboardItem temp = m_LeaderboardItem[j + 1];
+                        m_LeaderboardItem[j + 1] = m_LeaderboardItem[j];
+                        m_LeaderboardItem[j] = temp;
+                    }  
+                }  
+            }
+            
+            foreach (LeaderboardItem item in m_LeaderboardItem)
+            {
+                Debug.Log($"Name: {item.Name} --- Score: {item.Score}");
+            }
+        }
     }   
 }
