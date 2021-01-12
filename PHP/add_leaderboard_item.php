41645d61a945b0554d4ae9a50f9ca190715bed79
Lars <larsbeijaard@outlook.com>
2021-01-12 15:28:15 +0100

    Changed the PHP variable names

--- PHP/add_leaderboard_item.php
+++ PHP/add_leaderboard_item.php
@@ -5,13 +5,13 @@ require_once "connection.php";
 $data = $_POST["data"];
 $data = json_decode($data);
 
-$player_name = $data->name;
-$player_score = $data->score;
+$name = $data->name;
+$score = $data->score;
 
 try {
-    $add_player = "INSERT INTO `Bladiator`(`player_name`, `player_score`) VALUES (?, ?)";
+    $add_player = "INSERT INTO `Bladiator`(`name`, `score`) VALUES (?, ?)";
     $statement = $connection->prepare($add_player);
-    $statement->bind_param("si", $player_name, $player_score);
+    $statement->bind_param("si", $name, $score);
     $statement->execute();
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
