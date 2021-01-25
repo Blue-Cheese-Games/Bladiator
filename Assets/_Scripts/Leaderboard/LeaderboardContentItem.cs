5d6f75571c0a05c187a03a8cd69813f74060a8ca
Lars <larsbeijaard@outlook.com>
2021-01-25 13:26:18 +0100

    Remove redunded code

--- Assets/_Scripts/Leaderboard/LeaderboardContentItem.cs
+++ Assets/_Scripts/Leaderboard/LeaderboardContentItem.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 
 namespace Bladiator.Leaderboard
 {
     public class LeaderboardContentItem : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI m_PlayerName = null;
-        [SerializeField] private TextMeshProUGUI m_PlayerScore = null;
-        [SerializeField] private TextMeshProUGUI m_PlayerWave = null;
-
-        public LeaderboardContentItem(LeaderboardItemData itemData)
-        {
-            m_PlayerName.text = itemData.name;
-            m_PlayerScore.text = Convert.ToString(itemData.score);
-            m_PlayerWave.text = Convert.ToString(itemData.wave);
-        }
+        public TextMeshProUGUI PlayerName = null;
+        public TextMeshProUGUI PlayerScore = null;
+        public TextMeshProUGUI PlayerWave = null;
     }
 }
