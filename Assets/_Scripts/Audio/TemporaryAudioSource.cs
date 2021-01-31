aedc9bbd9a3b4103966fb818d6d1f174631b4a80
Roberto Peek <peekroberto@gmail.com>
2021-01-31 22:25:44 +0100

    added ",gif" to gitignore + push of missing commits

---
+++ Assets/_Scripts/Audio/TemporaryAudioSource.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bladiator.Sound
+{
+    public class TemporaryAudioSource : MonoBehaviour
+    {
+        [SerializeField] private AudioSource m_source;
+
+        private void Update()
+        {
+            if (!m_source.isPlaying)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
